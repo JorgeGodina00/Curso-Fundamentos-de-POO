a5beffc650221903f2951e3147e7b2cca9f5b57d
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-04-03 14:27:59 -0600

    se intentto completar la actividad
    probemas con las librerias de python, revisar urgente

--- tkintersqlite/USUARIOSCRUD.py
+++ tkintersqlite/USUARIOSCRUD.py
@@ -1,39 +1,136 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from tkinter import messagebox
+from controladorBD import *
 
-Ventana = Tk()
-Ventana.title("CRUD Usuarios")
-Ventana.geometry("500x300")
+# Creamos una instancia de tipo controlador
+controlador = controladorBD()
 
-panel = ttk.Notebook(Ventana)
+# Procedemos a Guardar usando el metodo del objeto controlador
+def ejecutaInsert():
+    controlador.guardarUsuario(varNom.get(), varCor.get(), varCon.get())
+    
+#Funcion para buscar un Usuario
+def ejecutaSelectU():
+    rsUsuario = controlador.consultarUsuario(varBus.get())
+
+    if rsUsuario:
+        textBus.delete("1.0","end")
+        # usuario_data = str(rsUsuario[0])
+        # Extraemos solamente nombre y correo
+        usuario_data = f"Nombre: {rsUsuario[0][1]}\nCorreo: {rsUsuario[0][2]}"
+        textBus.insert(tk.INSERT, usuario_data) 
+    
+    else:
+        messagebox.showinfo("Ojito", "Usuario no registrado en la BD")
+        textBus.delete("1.0","end")
+        return
+    
+# Definimos una función para consultar todos los usuarios en la base de datos
+def ejecutaSelectA():
+    rsUsuarios = controlador.consultarUsuarios()
+    # Limpiamos el treeview
+    tree.delete(*tree.get_children())
+    # Insertamos los datos en el treeview
+    for usuario in rsUsuarios:
+        tree.insert("", tk.END, values=usuario)
+        
+
+# Funcion para actualizar un usuario
+    
+
+# Creamos la ventana principal
+ventana = Tk()
+ventana.title("CRUD de Usuarios")
+ventana.geometry("650x400")
+
+# Creamos el Notebook
+panel = ttk.Notebook(ventana, style='TNotebook')
 panel.pack(fill="both", expand="yes")
 
-pest1 = ttk.Frame(panel)
-pest2 = ttk.Frame(panel)
-pest3 = ttk.Frame(panel)
-pest4 = ttk.Frame(panel)
+# Creamos un estilo para el Notebook
+estilo = ttk.Style()
+estilo.configure('TNotebook', tabposition='n')
 
-panel.add(pest1, text = "Formulario de Usuarios")
-panel.add(pest2, text = "Buscar Usuario")
-panel.add(pest3, text = "Consultar Usuarios")
-panel.add(pest4, text = "Actualizar Usuario")
+# Creamos las pestañas
+pestana1 = ttk.Frame(panel)
+pestana2 = ttk.Frame(panel)
+pestana3 = ttk.Frame(panel)
+pestana4 = ttk.Frame(panel)
 
-#Pest1: Formulario de Usuarios
-titulo = Label(pest1, text = "Registro de Usuarios", fg = "Blue", font = ("Modern", 18)).pack()
+# Agregamos las pestañas al Notebook
+panel.add(pestana1, text="Formulario de usuario")
+panel.add(pestana2, text="Buscar Usuario")
+panel.add(pestana3, text="Consultar Usuarios")
+panel.add(pestana4, text="Actualizar Usuario")
+
+
+# Pestaña1: Formulario de Usuario
+titulo = Label(pestana1, text="Registro Usuarios", fg="blue",  font=("Arial", 20, "bold"))
+titulo.pack()
 
 varNom = tk.StringVar()
-lblNom = Label(pest1, text="Nombre: ").pack()
-txtNom = Entry(pest1, textvariable=varNom).pack()
+lblNom = Label(pestana1, text="Nombre: ")
+lblNom.pack(pady=5)
+txtNom = Entry(pestana1, textvariable=varNom, width=30)
+txtNom.pack()
 
 varCor = tk.StringVar()
-lblNom = Label(pest1, text="Correo: ").pack()
-txtNom = Entry(pest1, textvariable=varCor).pack()
+lblCor = Label(pestana1, text="Correo: ")
+lblCor.pack(padx=5)
+txtCor = Entry(pestana1, textvariable=varCor, width=30)
+txtCor.pack()
 
+lblCon = Label(pestana1, text="Contraseña:")
+lblCon.pack(pady=5)
 varCon = tk.StringVar()
-lblNom = Label(pest1, text="Contrase;a: ").pack()
-txtNom = Entry(pest1, textvariable=varCon).pack()
+txtCon = Entry(pestana1, textvariable=varCon, width=30, show="*")
+txtCon.pack()
 
-btnGuardar = Button(pest1, text="Guardar Usuario").pack()
+btnGuardar = Button(pestana1, text="Guardar usuario", command=ejecutaInsert, bg="#008CBA", fg="white")
+btnGuardar.pack(pady=10)
 
-Ventana.mainloop()
+# Creamos los elementos para la pestaña 2 (Buscar usuario)
+titulo2= Label(pestana2, text="Buscar usuario", font=("Arial", 20, "bold"))
+titulo2.pack(pady=10)
+
+varBus = tk.StringVar()
+lblid = Label(pestana2, text="Identificador de Usuario: ")
+lblid.pack(pady=5)
+
+txtid = Entry(pestana2, textvariable=varBus, width=30)
+txtid.pack()
+
+btnBusqueda = Button(pestana2, text="Buscar usuario", command=ejecutaSelectU, bg="#008CBA", fg="white")
+btnBusqueda.pack(pady=10)
+
+
+subBus= Label(pestana2, text="Usuario: ", font=("Arial", 10, "bold"))   
+subBus.pack()
+
+textBus = Text(pestana2, width=40, height=5)
+textBus.pack(pady=10)
+
+
+# Creamos los elementos para la pestaña 3 (Consultar usuarios)
+titulo3 = Label(pestana3, text="Consultar usuarios", font=("Arial", 20, "bold"))
+titulo3.pack(pady=10)
+
+# Creamos un Treeview
+tree = ttk.Treeview(pestana3, columns=(1,2,3), show="headings", height="5")
+tree.pack()
+
+# Creamos las columnas del Treeview
+tree.heading(1, text="ID")
+tree.heading(2, text="Nombre")
+tree.heading(3, text="Correo")
+
+# Creamos un botón para consultar todos los usuarios
+btnConsulta = Button(pestana3, text="Consultar usuarios", command=ejecutaSelectA, bg="#008CBA", fg="white")
+btnConsulta.pack(pady=10)
+
+
+
+
+ventana.mainloop()
--- tkintersqlite/controladorBD.py
+++ tkintersqlite/controladorBD.py
@@ -1,6 +1,105 @@
-#1 llamar a la conexion
-conx = self.conexionDB()
+from tkinter import messagebox
+import sqlite3
+import bcrypt 
 
-#2. Revisar parametros vacio
-if(nom == "" or cor == "" or con ==""):
+class controladorBD:
     
+    def __init__(self):
+        pass
+        
+    #1. preparamos la conexion para usarla cuando sea necesario
+    def conexionBD(self):
+        try:
+            conexion = sqlite3.connect("C:/Users/reach/OneDrive/Documentos/POO184/tkinterSqlite/DBUsuarios.db")
+            print("conectado BD")
+            return conexion
+        except sqlite3.OperationalError:
+            print("No se pudo conectar")
+      
+    #Metodo para Insertar      
+    def guardarUsuario(self, nom, cor, con):
+        #1. llamar a la conexion
+        conx = self.conexionBD()
+        
+        #2. Revisar parametros vacios
+        if(nom == "" or cor == "" or con == ""):
+            messagebox.showwarning("Aguas!!", "Revisa este show")
+            conx.close()
+        else:
+            #3. Preparamos los datos y el querySQL
+            cursor = conx.cursor()
+            conH= self.encriptarCon(con)
+            
+            #validad si el correo ya esta registrado
+            sqlSelect = "select * from TBRegistrados where Correo=?"
+            cursor.execute(sqlSelect, (cor,))
+            rsUsuario = cursor.fetchall()
+            
+            if len(rsUsuario) > 0:
+                messagebox.showerror("Error", "El correo ya esta registrado")
+                return
+            
+            datos = (nom, cor, conH)
+            qrInsert = "insert into TBRegistrados(Nombre, Correo, Contraseñas) values(?,?,?)"
+            
+            #4. Ejecutamos la consulta y cerramos la conexion
+            cursor.execute(qrInsert, datos)
+            conx.commit()
+            conx.close()
+            messagebox.showinfo("Registro", "Registro exitoso")
+            
+    #Metodo para encryptar
+    def encriptarCon(self, con):
+        conPlana= con
+        conPlana= conPlana.encode() #convertimos con a bytes
+        sal= bcrypt.gensalt()
+        
+        conHa= bcrypt.hashpw(conPlana,sal)
+        print(conHa)
+        return conHa
+    
+    def consultarUsuario(self, id):
+        #1. preparar la conexion
+        conx= self.conexionBD()
+            
+        #2. verificar el ID no este vacio
+        if id =="":
+            messagebox.showwarning("Cuidado", "Id vacio con escribe un valor")
+         
+            return
+        else:
+            try:
+                #4. Preparar lo necesario para el select
+                cursor= conx.cursor()
+                sqlSelect="select * from TBRegistrados where id=?"
+                    
+                #5. Ejecutar la consulta y recuperar los datos
+                cursor.execute(sqlSelect, (id,))
+                rsUsuario = cursor.fetchall()
+                
+                #6. cerrar conexion y devolver los datos
+                print(rsUsuario)
+                conx.close()
+                return rsUsuario
+            except Exception as ex:
+                messagebox.showwarning("Error", str(ex))
+                conx.close()
+                return None
+            
+    def consultarUsuarios(self):
+        #1. preparar la conexion
+        conx= self.conexionBD()
+        
+        #2. Preparar lo necesario para el select
+        cursor= conx.cursor()
+        sqlSelect="select * from TBRegistrados"
+                
+                #3. Ejecutar la consulta y recuperar los datos
+        cursor.execute(sqlSelect)
+        rsUsuarios = cursor.fetchall()
+                
+        #4. cerrar conexion y devolver los datos
+        #print(rsUsuarios)
+        conx.close()
+        return rsUsuarios
+        
